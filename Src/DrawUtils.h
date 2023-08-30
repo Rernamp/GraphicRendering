c4f36f54d55fe53b5af4a79c473913995af7167a
rernamp <renamp100@gmail.com>
2023-08-30 22:26:02 +0300

    start draw triangle

--- Src/DrawUtils.h
+++ Src/DrawUtils.h
@@ -9,8 +9,63 @@
 struct Vector2 {
     Vector2(int x, int y) : x(x), y(y) {
     }
-    int x;
-    int y;
+    Vector2() {}
+
+    bool operator == (Vector2 value) {
+        return (x == value.x) && (y == value.y);
+    }
+
+    int x = 0;
+    int y = 0;
+};  
+
+class MoveByLine {
+public:
+    MoveByLine(Vector2 start, Vector2 stop) : _start(start), _stop(stop) {
+        _pointOnLine =  _start;
+        if (std::abs(_start.x - _stop.x) < std::abs(_start.y - _stop.y)) {
+            std::swap(_start.x, _start.y);
+            std::swap(_stop.x , _stop.y);
+            _steep = true;
+        }
+        if (start.x > stop.x) {
+            std::swap(_start.x, _stop.x);
+            std::swap(_start.y, _stop.y);
+        }
+
+        _delta.x = _stop.x - _start.x;
+        _delta.y = _stop.y - _start.y;
+        
+        _deltaError = std::abs(_delta.y)*2;            
+    }
+
+    Vector2 doNext() {
+        Vector2 result = _pointOnLine;
+        if (_steep) {
+            std::swap(_pointOnLine.x, _pointOnLine.y);
+        }
+        _error += _deltaError;
+
+        if (_error > _delta.x) {
+            _pointOnLine.y += (_stop.y > _start.y ? 1 : -1);
+            _error -= _delta.x*2;
+        }        
+        ++_pointOnLine.x;
+
+        if (_steep) {
+            std::swap(_pointOnLine.x, _pointOnLine.y);
+        }
+
+        return result;
+    } 
+private:
+    Vector2 _start;
+    Vector2 _stop;
+    bool _steep = false;
+    Vector2 _delta {};
+    Vector2 _pointOnLine {};
+    int _deltaError = 0;
+    int _error = 0;
 };
 
 class Utils {
@@ -57,10 +112,49 @@ public:
 
         std::array<Vector2, 3> points {t0, t1, t2};
 
+        
         std::sort(begin(points), end(points), [](const auto& left, const auto& right){
-            return left.x > right.x;
+            return left.y < right.y;
         });
 
+        int deltaY = points[2].y - points[0].y;
+
+        std::sort(begin(points), end(points), [](const auto& left, const auto& right){
+            return left.x < right.x;
+        });
+
+        int deltaX = points[2].x - points[0].x;
+
+        if (deltaY > deltaX) {
+            std::sort(begin(points), end(points), [](const auto& left, const auto& right){
+                return left.y < right.y;
+            });
+            MoveByLine bigLine {points[0], points[2]};
+            MoveByLine lessLine {points[0], points[1]};
+            for (int y = points[0].y; y <= points[2].y; y++) {
+                Vector2 point1 = bigLine.doNext();
+                Vector2 point2 = lessLine.doNext();
+                if (point2 == points[1]) {
+                    lessLine = {points[1], points[2]};
+                }
+                line(point1, point2, image, color);
+            }
+        } else {
+            MoveByLine bigLine {points[0], points[2]};
+            MoveByLine lessLine {points[0], points[1]};
+            for (int x = points[0].x; x <= points[2].x; x++) {
+                auto point1 = bigLine.doNext();
+                auto point2 = lessLine.doNext();
+                if (point2 == points[1]) {
+                    lessLine = {points[1], points[2]};
+                }
+                line(point1, point2, image, color);
+            }
+        }
+
+
+
+        
         
 
         
